cbb69a3735921265c7cc44a93029f502b7f162b4
Shobhan Mamidala <shobhan546@gmail.com>
2018-01-24 15:16:00 +0530

    first commit

---
+++ src/main/java/com/nisum/springbatch/Main.java
@@ -0,0 +1,32 @@
+package com.nisum.springbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionException;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	@SuppressWarnings("resource")
+	public static void main(String areg[]){
+		
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-batch-context.xml");
+		
+		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+		Job job = (Job) context.getBean("examResultJob");
+	 System.out.println("jobb");
+		try {
+			JobExecution execution = jobLauncher.run(job, new JobParameters());
+			System.out.println("Job Exit Status : "+ execution.getStatus());
+	 System.out.println("**");
+		} catch (JobExecutionException e) {
+			System.out.println("Job ExamResult failed");
+			e.printStackTrace();
+		}
+	}
+
+}
